ccb78309dba65d15f81fc29b8894ac0006dfd15a
Yunus Emre Atmaz <yunusemreatmaz@gmail.com>
2023-10-04 17:28:44 +0300

    first commit

---
+++ ders1.java
@@ -0,0 +1,16 @@
+public class ders1 { 
+    public static void main(String[] args){
+
+        System.out.println("Hello Ece"); //print komutu
+        System.out.println("Hello");
+        System.out.println("Merhaba ben Mustafa" + "\nSizlerle birliklte java ogreniyorum");
+        System.out.println(10 + "456");
+        System.out.println(10*25);
+        System.out.print("\n");
+        System.out.println("Eceee");
+        System.out.println("Merhaba \t Dünya");  //bir tab boşluk \t
+        
+
+    }
+
+}
---
+++ ders2.java
@@ -0,0 +1,43 @@
+import javax.xml.transform.Source;
+
+public class ders2 {
+    public static void main(String[] args) {
+        
+        byte abc = 100;
+        System.out.println("Byte değeri :" + abc);
+
+        short shortDegiskeni = 1000;
+        System.out.println(shortDegiskeni);
+
+        int intDeger = 14545;
+        System.out.println("Intager Deger : " + intDeger );
+
+
+        long longDeger = 107687475;
+        System.out.println("Long Deger: " + longDeger);
+
+        //************************************************************** */
+
+        float floatDegisken = 10.462F;   //float olduğunu F veya f ile ifade etmen gerek
+        System.out.println(floatDegisken);
+
+        double doubleSayi = 3;
+        System.out.println(doubleSayi);
+
+        doubleSayi = 45.65;        //önceden tanımlanan değişkeni böyle değiştirebiliriz
+        System.out.println(doubleSayi);
+
+        //**************************************************************** */
+
+        char karakter = 'u' ;
+        System.out.println(karakter);
+
+        boolean mantik = true;
+        System.out.println(mantik);
+
+
+        String str = "yunus emre";
+        System.out.println(str);
+        
+    }
+}
